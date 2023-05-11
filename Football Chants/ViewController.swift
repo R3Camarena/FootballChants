08f15a53309c530f0752cd00e5aa1bcd0ff7d9af
Ricardo Camarena <ricky_camarena@hotmail.com>
2023-05-10 18:56:13 -0600

    Initial Commit

---
+++ Football Chants/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Football Chants
+//
+//  Created by Ricardo Camarena on 10/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
