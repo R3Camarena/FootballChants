d4fb7ddd56eb705642d1623ac30a4578d207008b
Ricardo Camarena <ricky_camarena@hotmail.com>
2023-05-11 18:34:07 -0600

    feat: Added the chants as audio when tapping on the play button of each team item.

--- Football Chants/ViewControllers/ChantsViewController.swift
+++ Football Chants/ViewControllers/ChantsViewController.swift
@@ -24,6 +24,7 @@ class ChantsViewController: UIViewController {
     }()
     
     private lazy var teamsViewModel = TeamsViewModel()
+    private lazy var audioManagerViewModel = AudioManagerViewModel()
 
     // MARK: - Lifecycle
     
@@ -81,6 +82,7 @@ extension ChantsViewController: UITableViewDataSource {
 extension ChantsViewController: TeamTableViewCellDelegate {
     
     func didTapPlayback(for team: Team) {
+        audioManagerViewModel.playback(team)
         teamsViewModel.togglePlayback(for: team)
         tableView.reloadData()
     }
